cb61757a805eb1dd719caf1591049df6b9bfd9af
Michael Lee <xiaoguang_li@qq.com>
2016-04-21 18:45:42 +0800

    Project initialize

---
+++ MonsterDefend/MonsterDefend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonsterDefend
+//
+//  Created by LeeMichael on 4/21/16.
+//  Copyright © 2016 MichaelLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MonsterDefend/MonsterDefend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonsterDefend
+//
+//  Created by LeeMichael on 4/21/16.
+//  Copyright © 2016 MichaelLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
